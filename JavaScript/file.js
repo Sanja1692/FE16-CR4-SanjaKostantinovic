5ad9643b1f0b749f6f6a806cad2b5bc0e8f21f11
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-06-03 21:55:04 +0200

    cards

--- JavaScript/file.js
+++ JavaScript/file.js
@@ -1,19 +1,69 @@
-// console.log("works");
+const taskList = JSON.parse(tasks);
+// console.log(tasks);
+const taskCards = document.getElementById("allcards");
 
-let taskList = JSON.parse(tasks);
-let taskCards = document.getElementById(".allcards");
-
-function printInfo = () => {
+printData = () => {
     taskCards.innerHTML = "";
-    taskList.forEach((tasks) => {
-        taskCards.innerHTML += `<div class="container book-list">
-        //     <div id="img " style="width: 18rem">${tasks.image}</div>
-        //     <div id="result" class="row">
-        //         <p class="card-title" id="taskName">${tasks.taskName}</p>
-        //         <p id="description">${tasks.description}</p>
-        //         <p id="description">${tasks.dueTime}</p>
-        //     </div>
-        // </div>`
+    taskList.forEach((task) => {
+        taskCards.innerHTML += `    <div class="container mt-3" id="allcards">
+                <div class="card col-lg-3 col-md-5 col-sm-10 cardmod shadow-lg mt-0 mb-5" style="width: 18rem;">
+                <div class="justify-content-between d-flex pt-3 pb-3">
+        <div class="leftside ps-2">
+            <a href="#" class="btn btn-info pt-1 pb-1 text-white">Task</a>
+        </div>
+        <div class="rightside">
+            <img src="pics/bookmark.png" alt="" width="20" height="16">
+            <img src="pics/dot.png" alt="" width="20" height="16">
+        </div>
+    </div>
+                <img src="${task.image}" class="card-img-top p-2" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title text-center">${task.taskName}</h5>
+                  <p class="card-text text-center">${task.description}</p>
+                </div>
+                <div class="d-flex border-top pt-3">
+        <p class="m-0 ms-2 align-self-center"><p class="m-0 align-self-center"><img src="pics/important.png" class="importantbutton" alt="" width="20" height="20"> Priority level: </p>
+            <p class="p-1 rounded m-0 importance" id="test3">${task.importance}</p>
+        </p>
+    </div>
+    <div class="border-bottom pt-2 ms-2">
+        <p><img src="pics/calender.png" alt="" width="20" height="20"> Deadline: ${task.dueTime}</p>
+    </div>
+    <div class="text-end pt-3 pe-2">
+            <p class="btn btn-danger"><img src="pics/trash.png" alt="" width="20" height="20">Delete</p>
+            <p class="btn btn-success"><img src="pics/check.png" alt="" width="20" height="20">Done</p>
+        </div>
+          </div></div>
+
+        </div>`
     });
 };
-printInfo();
+printData();
+// <div class="card col-lg-3 col-md-5 col-sm-10 cardmod shadow-lg mt-0 mb-5" id="importance1">
+//     <div class="justify-content-between d-flex pt-3 pb-3">
+//         <div class="leftside">
+//             <a href="#" class="btn btn-info pt-1 pb-1 text-white">Task</a>
+//         </div>
+//         <div class="rightside">
+//             <img src="pics/bookmark.png" alt="" width="20" height="16">
+//             <img src="pics/dot.png" alt="" width="20" height="16">
+//         </div>
+//     </div>
+//     <img src="$task.image}" class="card-img-top" alt="">
+//     <div class="card-body">
+//         <h5 class="card-title text-center">${task.taskName}</h5>
+//         <p class="card-text text-center">${task.description}</p>
+//     </div>
+//     <div class="d-flex border-top pt-3">
+//         <p class="m-0 align-self-center"><p class="m-0 align-self-center"><img src="pics/important.png" class="importantbutton" alt="" width="20" height="20"> Priority level: </p>
+//             <p class="p-1 rounded m-0 importance" id="test3">${task.importance}</p>
+//         </p>
+//     </div>
+//     <div class="border-bottom pt-2">
+//         <p><img src="pics/calender.png" alt="" width="20" height="20"> Deadline: ${task.date}</p>
+//     </div>
+//     <div class="text-end pt-3">
+//         <p class="btn btn-danger"><img src="pics/trash.png" alt="" width="20" height="20">Delete</p>
+//         <p class="btn btn-success"><img src="pics/check.png" alt="" width="20" height="20">Done</p>
+//     </div>
+// </div>
